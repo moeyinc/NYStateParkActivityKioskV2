63754da07b34d261aef6a9285b5904a94ad9f982
Moey Design <5884seiya@gmail.com>
2019-01-21 15:59:22 -0500

    scafold base

---
+++ app/nuxt/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'google'
+  ],
+  // add your custom rules here
+  rules: {}
+}
