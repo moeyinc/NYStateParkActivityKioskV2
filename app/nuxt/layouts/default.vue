0b34fdcc49e3192ba4316e74eaa791bff3afe09b
Seiya Kobayashi <5884seiya@gmail.com>
2019-10-07 14:49:36 -0400

    foundation setup

--- app/nuxt/layouts/default.vue
+++ app/nuxt/layouts/default.vue
@@ -1,3 +1,32 @@
 <template>
-  <nuxt />
+  <div
+    class="default-layout"
+    :style="{
+      backgroundColor: backgroundColor,
+      color: textColor,
+    }"
+  >
+    <nuxt />
+  </div>
 </template>
+
+<script>
+import { generalSettings } from '@/test-data';
+export default {
+  computed: {
+    backgroundColor () {
+      return generalSettings.backgroundColor.hex;
+    },
+    textColor () {
+      return generalSettings.textColor.hex;
+    },
+  },
+};
+</script>
+
+<style scoped lang="stylus">
+.default-layout
+  width: 1920px
+  height: 1080px
+  overflow: hidden
+</style>
---
+++ app/nuxt/test-data.js
@@ -0,0 +1,205 @@
+export const generalSettings = {
+  backgroundColor: { hex: '#FFE7CC' },
+  textColor: { hex: '#0A3C5B' },
+  kioskTitle: 'THINGS TO DO',
+  parkLogoFilename: 'park-logo.svg',
+  screenTimeoutInSeconds: 300,
+};
+
+export const activities = [
+  {
+    buttonLabel: 'HIKING',
+    pageTitle: 'HIKING',
+    mainColor: { hex: '#B74F59' },
+    subColor: { hex: '#C3878C' },
+    iconFilename: 'hiking-icon.svg',
+    tabItems: [
+      {
+        templateType: 'GENERAL',
+        tabLabel: 'INFORMATION',
+      },
+    ],
+  },
+  {
+    buttonLabel: 'HIKING',
+    pageTitle: 'HIKING',
+    mainColor: { hex: '#AB5360' },
+    subColor: { hex: '#BB8A90' },
+    iconFilename: 'hiking-icon.svg',
+    tabItems: [
+      {
+        templateType: 'GENERAL',
+        tabLabel: 'INFORMATION',
+      },
+    ],
+  },
+  {
+    buttonLabel: 'HIKING',
+    pageTitle: 'HIKING',
+    mainColor: { hex: '#90586E' },
+    subColor: { hex: '#AB8C99' },
+    iconFilename: 'hiking-icon.svg',
+    tabItems: [
+      {
+        templateType: 'GENERAL',
+        tabLabel: 'INFORMATION',
+      },
+    ],
+  },
+  {
+    buttonLabel: 'HIKING',
+    pageTitle: 'HIKING',
+    mainColor: { hex: '#6C5E80' },
+    subColor: { hex: '#968FA4' },
+    iconFilename: 'hiking-icon.svg',
+    tabItems: [
+      {
+        templateType: 'GENERAL',
+        tabLabel: 'INFORMATION',
+      },
+    ],
+  },
+  {
+    buttonLabel: 'HIKING',
+    pageTitle: 'HIKING',
+    mainColor: { hex: '#40638F' },
+    subColor: { hex: '#7E92AE' },
+    iconFilename: 'hiking-icon.svg',
+    tabItems: [
+      {
+        templateType: 'GENERAL',
+        tabLabel: 'INFORMATION',
+      },
+    ],
+  },
+  {
+    buttonLabel: 'HIKING',
+    pageTitle: 'HIKING',
+    mainColor: { hex: '#35738C' },
+    subColor: { hex: '#7B9CAD' },
+    iconFilename: 'hiking-icon.svg',
+    tabItems: [
+      {
+        templateType: 'GENERAL',
+        tabLabel: 'INFORMATION',
+      },
+    ],
+  },
+  {
+    buttonLabel: 'HIKING',
+    pageTitle: 'HIKING',
+    mainColor: { hex: '#2D8287' },
+    subColor: { hex: '#79A6AA' },
+    iconFilename: 'hiking-icon.svg',
+    tabItems: [
+      {
+        templateType: 'GENERAL',
+        tabLabel: 'INFORMATION',
+      },
+    ],
+  },
+  {
+    buttonLabel: 'HIKING',
+    pageTitle: 'HIKING',
+    mainColor: { hex: '#1E9580' },
+    subColor: { hex: '#76B3A6' },
+    iconFilename: 'hiking-icon.svg',
+    tabItems: [
+      {
+        templateType: 'GENERAL',
+        tabLabel: 'INFORMATION',
+      },
+    ],
+  },
+  {
+    buttonLabel: 'HIKING',
+    pageTitle: 'HIKING',
+    mainColor: { hex: '#08A775' },
+    subColor: { hex: '#70BEA0' },
+    iconFilename: 'hiking-icon.svg',
+    tabItems: [
+      {
+        templateType: 'GENERAL',
+        tabLabel: 'INFORMATION',
+      },
+    ],
+  },
+  {
+    buttonLabel: 'HIKING',
+    pageTitle: 'HIKING',
+    mainColor: { hex: '#74AA61' },
+    subColor: { hex: '#A6C295' },
+    iconFilename: 'hiking-icon.svg',
+    tabItems: [
+      {
+        templateType: 'GENERAL',
+        tabLabel: 'INFORMATION',
+      },
+    ],
+  },
+  {
+    buttonLabel: 'HIKING',
+    pageTitle: 'HIKING',
+    mainColor: { hex: '#A2AA50' },
+    subColor: { hex: '#BFC38C' },
+    iconFilename: 'hiking-icon.svg',
+    tabItems: [
+      {
+        templateType: 'GENERAL',
+        tabLabel: 'INFORMATION',
+      },
+    ],
+  },
+  {
+    buttonLabel: 'HIKING',
+    pageTitle: 'HIKING',
+    mainColor: { hex: '#C7A83E' },
+    subColor: { hex: '#D4C282' },
+    iconFilename: 'hiking-icon.svg',
+    tabItems: [
+      {
+        templateType: 'GENERAL',
+        tabLabel: 'INFORMATION',
+      },
+    ],
+  },
+  {
+    buttonLabel: 'HIKING',
+    pageTitle: 'HIKING',
+    mainColor: { hex: '#EEA424' },
+    subColor: { hex: '#EBC074' },
+    iconFilename: 'hiking-icon.svg',
+    tabItems: [
+      {
+        templateType: 'GENERAL',
+        tabLabel: 'INFORMATION',
+      },
+    ],
+  },
+  {
+    buttonLabel: 'HIKING',
+    pageTitle: 'HIKING',
+    mainColor: { hex: '#CE7024' },
+    subColor: { hex: '#D39D6E' },
+    iconFilename: 'hiking-icon.svg',
+    tabItems: [
+      {
+        templateType: 'GENERAL',
+        tabLabel: 'INFORMATION',
+      },
+    ],
+  },
+  {
+    buttonLabel: 'HIKING',
+    pageTitle: 'HIKING',
+    mainColor: { hex: '#AF4324' },
+    subColor: { hex: '#BD7E6A' },
+    iconFilename: 'hiking-icon.svg',
+    tabItems: [
+      {
+        templateType: 'GENERAL',
+        tabLabel: 'INFORMATION',
+      },
+    ],
+  },
+];
