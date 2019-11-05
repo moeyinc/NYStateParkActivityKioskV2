4e6f3c58ad5e2a7153086ad7bc3b9bf1263a1611
Seiya Kobayashi <5884seiya@gmail.com>
2019-11-05 16:07:04 -0500

    create HomeActivityListItem

--- app/nuxt/components/home/HomeActivityListItem.vue
+++ app/nuxt/components/home/HomeActivityListItem.vue
@@ -2,11 +2,26 @@
   <div
     class="home-activity-list-item"
     :style="buttonStyle"
+    @mousedown="active = true"
+    @mouseup="active = false"
+    @mouseleave="active = false"
+    @click="$emit('click')"
   >
     <div class="inner">
       <simple-svg
         :src="require('@media/icons/' + activityIconFilename)"
+        fill-class-name="fill-to-change"
+        :fill="svgColor"
+        stroke-class-name="stroke-to-change"
+        :stroke="svgColor"
+        :height="svgHeight"
       />
+      <div
+        class="button-label"
+        :style="labelStyle"
+      >
+        {{ activityButtonLabel }}
+      </div>
     </div>
   </div>
 </template>
@@ -34,13 +49,27 @@ export default {
       default: '',
     },
   },
+  data: () => ({
+    active: false,
+  }),
   computed: {
     buttonStyle () {
       return {
         width: this.buttonWidth + 'px',
         height: this.buttonHeight + 'px',
         margin: this.buttonMargin,
-        backgroundColor: this.activityMainColor,
+        backgroundColor: this.active ? 'white' : this.activityMainColor,
+      };
+    },
+    svgColor () {
+      return this.active ? this.activityMainColor : 'white';
+    },
+    svgHeight () {
+      return (this.buttonHeight * 0.6) + 'px';
+    },
+    labelStyle () {
+      return {
+        color: this.svgColor,
       };
     },
   },
@@ -48,6 +77,18 @@ export default {
 </script>
 
 <style scoped lang="stylus">
+@import '~@styles/fonts'
+
 .home-activity-list-item
   border-radius: 50px
+  .inner
+    display: flex
+    flex-direction: column
+    justify-content: center
+    align-items: center
+    height: 100%
+    .button-label
+      font-family: $title-font
+      margin-top: 20px
+      font-size: 28px
 </style>
