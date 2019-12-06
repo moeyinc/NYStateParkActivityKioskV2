e5cf7bcdffedc1deede3ec0aeb6e873ecab0c75a
Seiya Kobayashi <5884seiya@gmail.com>
2019-12-06 17:02:15 -0500

    update seeding data for activity

--- cms/schema/activity.js
+++ cms/schema/activity.js
@@ -32,106 +32,106 @@ module.exports = {
     {
       data: {
         order: 1,
-        buttonLabel: "Activity 1",
-        pageTitle: "Activity 1"
+        buttonLabel: "HIKING",
+        pageTitle: "HIKING"
       },
     },
     {
       data: {
         order: 2,
-        buttonLabel: "Activity 2",
-        pageTitle: "Activity 2"
+        buttonLabel: "BIKING",
+        pageTitle: "BIKING"
       },
     },
     {
       data: {
         order: 3,
-        buttonLabel: "Activity 3",
-        pageTitle: "Activity 3"
+        buttonLabel: "ROCK CLIMBING",
+        pageTitle: "ROCK CLIMBING"
       },
     },
     {
       data: {
         order: 4,
-        buttonLabel: "Activity 4",
-        pageTitle: "Activity 4"
+        buttonLabel: "PICNIC SHELTER",
+        pageTitle: "PICNIC SHELTER"
       },
     },
     {
       data: {
         order: 5,
-        buttonLabel: "Activity 5",
-        pageTitle: "Activity 5"
+        buttonLabel: "CAMPGROUND",
+        pageTitle: "CAMPGROUND"
       },
     },
     {
       data: {
         order: 6,
-        buttonLabel: "Activity 6",
-        pageTitle: "Activity 6"
+        buttonLabel: "BIRDING",
+        pageTitle: "BIRDING"
       },
     },
     {
       data: {
         order: 7,
-        buttonLabel: "Activity 7",
-        pageTitle: "Activity 7"
+        buttonLabel: "FISHING",
+        pageTitle: "FISHING"
       },
     },
     {
       data: {
         order: 8,
-        buttonLabel: "Activity 8",
-        pageTitle: "Activity 8"
+        buttonLabel: "DISC GOLF",
+        pageTitle: "DISC GOLF"
       },
     },
     {
       data: {
         order: 9,
-        buttonLabel: "Activity 9",
-        pageTitle: "Activity 9"
+        buttonLabel: "WINTER ACTIVITIES",
+        pageTitle: "WINTER ACTIVITIES"
       },
     },
     {
       data: {
         order: 10,
-        buttonLabel: "Activity 10",
-        pageTitle: "Activity 10"
+        buttonLabel: "GEOCACHING",
+        pageTitle: "GEOCACHING"
       },
     },
     {
       data: {
         order: 11,
-        buttonLabel: "Activity 11",
-        pageTitle: "Activity 11"
+        buttonLabel: "PLAYGROUNDS",
+        pageTitle: "PLAYGROUNDS"
       },
     },
     {
       data: {
         order: 12,
-        buttonLabel: "Activity 12",
-        pageTitle: "Activity 12"
+        buttonLabel: "FOOD",
+        pageTitle: "FOOD"
       },
     },
     {
       data: {
         order: 13,
-        buttonLabel: "Activity 13",
-        pageTitle: "Activity 13"
+        buttonLabel: "EVENT SPACE",
+        pageTitle: "EVENT SPACE"
       },
     },
     {
       data: {
         order: 14,
-        buttonLabel: "Activity 14",
-        pageTitle: "Activity 14"
+        buttonLabel: "PROGRAMS & EVENTS",
+        pageTitle: "PROGRAMS & EVENTS"
       },
     },
     {
       data: {
         order: 15,
-        buttonLabel: "Activity 15",
-        pageTitle: "Activity 15"
+        buttonLabel: "NATURE CENTER",
+        pageTitle: "NATURE CENTER"
       }
     }
   ]`,
